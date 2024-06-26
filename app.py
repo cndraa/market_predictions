7eac1c130bc73b45ad636dfc6d2302c63df86868
Azril Achmad <azril.achmad@smt-id.com>
2024-06-26 18:18:30 +0700

    Adjust promt text and response list of the market prediction action

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 # Access your API key as an environment variable.
-os.environ["GOOGLE_API_KEY"] = "AIzaSyC-HjO8_T88mEugQ6Pb2Ih8IiZRv9Te_NA"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyDPkQPzsnLh2uV_LSz9l93izD2MUP_z6U0"
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))# Choose a model that's appropriate for your use case.
 model = genai.GenerativeModel('gemini-1.5-flash')
 
@@ -27,6 +27,10 @@ def index():
     predict_text = None
     bottom_filtered = None
     top_filtered = None
+    analisa_harga = None
+    informasi_tambahan = ''
+    linkArr = []
+    image_kendaraan = ''
     
     if request.method == "POST":
         jenis_kendaraan = request.form["jenis_kendaraan"]
@@ -46,16 +50,35 @@ def index():
             nama_kendaraan = response.text
             
             # Analisis harga pasar
-            prompt_predict= f"Berikan Average Market Price untuk { jenis_kendaraan } Bekas {nama_kendaraan} tahun {tahun_kendaraan} wilayah {wilayah_kendaraan} dengan format sebagai berikut:\n1. Nama Kendaraan:\n2. Tahun Kendaraaan:\n3. Wilayah Kendaraan\n4. Harga Terendah: {{ Harga Terendah }}\n5. Harga Tertinggi: {{ Harga Tertinggi }}\n6. Sumber Analisa Harga: {{ Olx, Oto.com, dan sebagainya }}\n\nbuat secara singkat, tanpa catatan dan informasi tambahan, hanya isi formatnya saja\ndan tambahkan<br> di setiap  akhir kalimat"
+            prompt_predict= f"Berikan Average Market Price untuk { jenis_kendaraan } Bekas {nama_kendaraan} tahun {tahun_kendaraan} wilayah {wilayah_kendaraan} dengan format sebagai berikut:\[Harga Terendah]\n[Harga Tertinggi]\nlink presisi Sumber Analisa Harga (cukup 2 sumber berbeda dari olx dan oto yang menjual mobil tesebut per baris)\nKalimat Informasi Tambahan\n1 link image kendaraan tersebut\nlangsung isi format dengan jawabannya nya saja tidak perlu mencantumkan kembali : dan tambahkan <br> di setiap akhir kalimat\n\n contoh hasil nya harus selalu seperti ini:\n\nRp. 260.000.000\nRp. 300.000.000 <br>\nhttps://www.olx.co.id/items/toyota-innova-2-0-g-mt-2021-second-semarang_i2367454232 <br>\nhttps://www.oto.com/mobil-bekas/toyota/innova/2021/semarang/toyota-innova-20-g-mt-2021-semarang-28145824 <br>\nHarga mobil bekas Toyota Innova 2.0 G MT tahun 2021 di Semarang berkisar antara Rp. 260.000.000 hingga Rp. 300.000.000. Harga ini dapat bervariasi tergantung kondisi, kilometer, dan kelengkapan mobil. <br>\nhttps://[link image mobil (non-encrypted) yang dapat ditampilkan]"
             response_predict = model.generate_content(prompt_predict)
+            # print(response_predict.text)
             predict_text = response_predict.text
             array_text = response_predict.text.split('\n')  # --> ['Line 1', 'Line 2', 'Line 3']
+            print(response_predict.text)
+            print(array_text)
             bottom_filtered = ''
             top_filtered = ''
-            bottom_filtered = array_text[3].replace('4. Harga Terendah: ', '')
+            bottom_filtered = array_text[0]
             bottom_filtered = bottom_filtered.replace('<br>', '')
-            top_filtered = array_text[4].replace('5. Harga Tertinggi: ', '')
+            top_filtered = array_text[1]
             top_filtered = top_filtered.replace('<br>', '')
+            informasi_tambahan = array_text[0]
+
+            
+            
+            
+            # Mengambil list sumber analisa harga
+            def is_valid_url(url):
+                url_regex = re.compile(r'https?://(?:www\.)?[a-zA-Z0-9./]+')
+                return bool(url_regex.match(url))
+            
+            for x in array_text:
+                if is_valid_url(x):
+                    linkArr.append(x.replace('<br>',''))
+            
+        
+            
 
         except Exception as e:
             error_message = f"Terjadi kesalahan: {e}"
@@ -106,7 +129,11 @@ def index():
         wilayah_kendaraan = wilayah_kendaraan,
         predict_text = predict_text,
         bottom_filtered = bottom_filtered,
-        top_filtered = top_filtered
+        top_filtered = top_filtered,
+        linkArr1 = array_text[2].replace('<br>' ,''),
+        linkArr2 = array_text[3].replace('<br>' ,''),
+        informasi_tambahan = array_text[4].replace('<br>' ,''),
+        image_kendaraan = array_text[5].replace('<br>' ,'')
     )
 
 
